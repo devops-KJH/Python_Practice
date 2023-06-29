438d176a12df90ff1a0dca8991201d2665410f18
devops-KJH <jhcode93@gmail.com>
2023-06-30 00:31:53 +0900

    chapter1~4

---
+++ Chatpter04_연산과연산자/03_불린형.py
@@ -0,0 +1,6 @@
+print(2 > 3)
+print(15 < 30)
+print(1.5 >= 0)
+print(3 <= 3)
+print("야나두" == "야너두")
+print(10 != 10)
---
+++ Chatpter04_연산과연산자/04_논리연산.py
@@ -0,0 +1,3 @@
+print(4<6 and 10>=10)
+print("hi"!="hi" or "bye"=="bye")
+print(not 5==5)
